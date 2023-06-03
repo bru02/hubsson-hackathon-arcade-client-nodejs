bc2aae2968bc495347f40ece6fcc537d4b3daf0b
Brúnó Salomon <bru02.mail@gmail.com>
2023-06-03 14:37:02 +0200

    init

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,9 @@
 import { Configuration } from "../libs/model";
-
+export const playerId = 'encircler'
 export const config: Configuration = {
-	teamName: "<<TEAM NAME>>",
-	id: "<<TEAM ID>>",
-	secret: "<<TEAN SECRET>>",
-	serverUrl: "<<URL>>"
+	teamName: "Encircler",
+	id: playerId,
+	secret: "aa11986d-0d19-42df-b0d5-b555afa429f1",
+	serverUrl:
+		"https://hubsson-hackathon-team4.ashyground-b4876dbe.northeurope.azurecontainerapps.io",
 };
--- app/main.ts
+++ app/main.ts
@@ -1,18 +1,118 @@
-import { GameState, Direction } from "../libs/model";
+import {
+	GameState,
+	Direction,
+	PlayerCoordinates,
+	Coordinate,
+} from "../libs/model";
 import { GameClient } from "../libs/game-client";
 
-import { config } from "./config";
-
+import { config, playerId } from "./config";
 
 const gameClient = new GameClient(config);
 
+enum Goal {
+	Start,
+	Encircle,
+	Encircled,
+}
+let prevDir: Direction;
+let goal: Goal;
+
 gameClient.onGameStart((): void => {
-	console.log("Game started!");
+	console.log("Game fasfas!");
+	prevDir = Direction.DOWN;
+	goal = Goal.Start
 });
 
-gameClient.onGameUpdate((gameState: GameState): void => {
+gameClient.onGameUpdate((state: GameState): void => {
+	const me = state.players.find((p) => p.playerId === playerId);
+
+	const { x, y } = me.coordinates.at(-1);
+
+	function isObstacle(x, y) {
+		if (x < 0 || x === state.width || y < 0 || y === state.height)
+			return true;
+
+		for (let player of state.players) {
+			for (let i = player.coordinates.length - 1; i > 0; i--) {
+				const choords = player.coordinates[i];
+
+				if (
+					(choords.x === x && choords.y === y) ||
+					(player.playerId !== playerId && i === player.coordinates.length - 1 &&
+						isNeighbor({ x, y }, choords))
+				) {
+					return true;
+				}
+			}
+		}
+
+		return false
+	}
+
+	function isNeighbor(
+		{ x: x1, y: y1 }: Coordinate,
+		{ x: x2, y: y2 }: Coordinate
+	) {
+		return (
+			(x1 === x2 && Math.abs(y1 - y2) === 1) ||
+			(y1 === y2 && Math.abs(x1 - x2) === 1)
+		);
+	}
+
+	function wouldCollide(x: number, y: number, dir: Direction) {
+		switch (dir) {
+			case "Up":
+				return isObstacle(x, y - 1);
+			case "Down":
+				return isObstacle(x, y + 1);
+			case "Left":
+				return isObstacle(x - 1, y);
+			case "Right":
+				return isObstacle(x + 1, y);
+		}
+	}
+
+	function getMovesForPlayer(player: PlayerCoordinates): Direction[] {
+		const { x, y } = player.coordinates.at(-1);
+		console.log(x, y);
+		const ret = (["Up", "Down", "Left", "Right"] as Direction[]).filter(
+			(dir) => !wouldCollide(x, y, dir)
+		);
+		console.log(ret)
+		if (!ret.length) return [Direction.UP];
+		return ret;
+	}
+
+	function calculateDirectionTerritory(
+		dir: Direction,
+		player: PlayerCoordinates
+	) {}
+
 	console.log("Game State received");
-	gameClient.sendAction(Direction.UP, gameState.iteration);
+	const possibleMoves = getMovesForPlayer(me);
+
+	if (goal === Goal.Start && !possibleMoves.includes(Direction.DOWN)) {
+		goal = Goal.Encircle;
+		if (possibleMoves.length > 1) {
+			prevDir = x < state.width - x ? Direction.RIGHT : Direction.LEFT;
+		} else {
+			prevDir = possibleMoves[0];
+		}
+	} else if(goal === Goal.Encircle) {
+		
+
+	} else {
+		if (!possibleMoves.includes(prevDir)) {
+			prevDir =
+				possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+		}
+	}
+
+	console.log({goal, possibleMoves});
+	console.log(Goal.Start)
+
+	gameClient.sendAction(prevDir, state.iteration);
 });
 
 gameClient.run();
