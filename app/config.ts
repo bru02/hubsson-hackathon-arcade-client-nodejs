769fbf0bdc2404bfd9a59dbfcfefefe6aaca0b2b
Brúnó Salomon <bru02.mail@gmail.com>
2023-06-03 15:42:12 +0200

    fix

--- app/config.ts
+++ app/config.ts
@@ -5,5 +5,5 @@ export const config: Configuration = {
 	id: playerId,
 	secret: "aa11986d-0d19-42df-b0d5-b555afa429f1",
 	serverUrl:
-		"https://hubsson-hackathon-team4.ashyground-b4876dbe.northeurope.azurecontainerapps.io",
+		"https://hubsson-hackathon-production.ashyground-b4876dbe.northeurope.azurecontainerapps.io",
 };
